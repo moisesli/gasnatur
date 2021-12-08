7b459e9a51dac151aa143b03f22a598e729b2b68
moises <elnaufrago2009@gmail.com>
2021-12-08 12:45:29 -0500

    edit login

--- views/layouts/auth.blade.php
+++ views/layouts/auth.blade.php
@@ -5,11 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title')</title>
 
+  {{--Vue js--}}
+  <script src="https://unpkg.com/vue@next"></script>
+
   <!-- Tailwind Css -->
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+
+  {{--Add Js Login--}}
+  {{--<script src="/views/auth/login.js"></script>--}}
   
 </head>
 <body>
